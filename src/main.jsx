376bf4358855c55dc082ec885f7a0a8d3bb5c415
Yogesh Kr. Gupta <guptayogesh484.00@gmail.com>
2024-12-24 00:45:47 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ import './index.css'
 import 'lenis/dist/lenis.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode basename={import.meta.env.basename}>
+  <StrictMode>
     <App />
   </StrictMode>,
 )
