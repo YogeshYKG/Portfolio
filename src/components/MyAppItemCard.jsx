8f690e581a89fc1ef6aeb7dc416ab9af19d8b95f
Yogesh Kr. Gupta <guptayogesh484.00@gmail.com>
2024-12-29 19:16:17 +0530

    Updated typewrite effect, Project Card structure, added blur effects, Autoplat Swipe feature

--- src/components/MyAppItemCard.jsx
+++ src/components/MyAppItemCard.jsx
@@ -16,6 +16,7 @@ const MyAppItemCard = ({
     label,
     desc,
     tags,
+    alt,
     classes,
     onCardClick
 }) => {
@@ -24,34 +25,51 @@ const MyAppItemCard = ({
 
       <div className="">
         <div className="">
-          <div 
-              className={`relative p-4 rounded-2xl bg-zinc-800 hover:bg-zinc-700/50 active:bg-zinc-700/60 ring-1 ring-inset ring-zinc-50/5 transition-colors bg-cover bg-center-right bg-top  h-full min-h-[20rem]`}
-              style={{
-                  backgroundImage: `url(${imgSrc})`,
-                  
-              }}
-              onMouseEnter={() => setIsHovered(true)}
-              onMouseLeave={() => setIsHovered(false)}
-              onClick={() => onCardClick(iframeSrc)} // Pass iframeSrc to MyApp component
-          >
-            <div className={`absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-end items-center transition-all duration-1000 ${isHovered ? 'opacity-100' : 'opacity-0'}`}>
-                  <div className={`transform ${isHovered ? 'translate-y-[-4rem]' : 'translate-y-[0rem]'} transition-all duration-1600`}>
-                      <h3 className="text-zinc-100 text-xl title-1 mb-2">{label}</h3>
-                  </div>
-                  <div className="flex flex-wrap items-center gap-2">
-                        {tags.map((label, key) => (
-                            <span
-                                key={key}
-                                className="h-8 text-sm text-zinc-300 bg-zinc-50/5 grid items-center px-3 rounded-lg"
-                            >
-                                {label}
-                            </span>
-                        ))}
-                  </div>
-                  <div className={`transform ${isHovered ? 'translate-y-[-6rem]' : 'translate-y-[0rem]'} transition-all duration-2400`}>
-                      <p className="text-zinc-300 text-sm">{desc}</p>
-                  </div>
-            </div>
+            <div 
+                className={`relative p-4 rounded-2xl ring-4 ring-inset ring-sky-700 border border-sky-300 transition-all duration-500 bg-cover bg-top h-full min-h-[30rem]`}
+                style={{
+                    backgroundImage: `url(${imgSrc})`,
+                }}
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
+                onClick={() => onCardClick(iframeSrc)} // Pass iframeSrc to MyApp component
+            >
+                 <div
+                    className={`absolute inset-0 w-full h-full rounded-2xl ${isHovered ? 'opacity-100 filter blur-[3px] bg-cover bg-top' : 'opacity-0' } transition-all duration-1000`}
+                    style={{
+                        backgroundImage: `url(${imgSrc})`,
+                    }}
+                ></div>
+
+                    <div
+                        className={`absolute top-0 left-1/2 transform -translate-x-1/2 bg-cover bg-no-repeat w-4/5 h-3/5 rounded-2xl ring-4 ring-inset ring-sky-700 border border-sky-300 
+                            ${isHovered ? 'opacity-100 translate-y-6' : 'opacity-0 translate-y-0'} transition-all duration-1000`}
+                        style={{
+                            backgroundImage: `url(${imgSrc})`,
+                        }}
+                    ></div>
+                    
+                    <div className={`absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-end items-center transition-all duration-1000 ${isHovered ? 'opacity-100' : 'opacity-0'}`}>
+                        <div className={`transform ${isHovered ? 'translate-y-[-4rem]' : 'translate-y-[0rem]'} transition-all duration-1600`}>
+                            <h3 className="text-zinc-900 text-x2 title-1 mb-2 drop-shadow hover:text-x4 transition-all">
+                            {label}
+                            </h3>
+                        </div>
+                        <div className="flex flex-wrap items-center gap-2">
+                                {tags.map((label, key) => (
+                                    <span
+                                        key={key}
+                                        className="h-8 text-sm text-zinc-300 bg-zinc-50/5 hover:bg-zinc-800 hover:text-base transition-all grid items-center px-3 rounded-lg"
+                                    >
+                                        {label}
+                                    </span>
+                                ))}
+                        </div>
+                        <div className={`transform ${isHovered ? 'translate-y-[-6rem]' : 'translate-y-[0rem]'} transition-all duration-2400`}>
+                            <p className="text-zinc-300 text-sm typewriter hover:text-base transition-all">{desc}</p>
+                        </div>
+                    </div>
+
 
           </div>
         </div>
@@ -67,6 +85,7 @@ MyAppItemCard.propTypes = {
     label: PropTypes.string.isRequired,
     desc: PropTypes.string.isRequired,
     tags: PropTypes.array.isRequired, 
+    alt: PropTypes.string,
     classes: PropTypes.string,
     onCardClick: PropTypes.func.isRequired
 };
